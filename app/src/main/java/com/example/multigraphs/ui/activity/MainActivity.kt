d3d26dbe98971a3d53bf1d5140f39a6db5f8be12
JusupJoseph <busurmankulovzusup@gmail.com>
2022-12-16 13:51:43 +0600

    1

--- app/src/main/java/com/example/multigraphs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/multigraphs/ui/activity/MainActivity.kt
@@ -19,13 +19,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        this.setTheme(R.style.Theme_MultiGraphs)
         setupNavigation()
     }
 
     private fun setupNavigation() {
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
